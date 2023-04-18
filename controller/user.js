42efb56104506b7652a554f1c49024d4a6965b82
Tyrosius <florian.schoof@gmx.de>
2023-04-18 18:43:15 +0200

    working basic user login etc

--- controller/user.js
+++ controller/user.js
@@ -2,13 +2,14 @@ const {hashPassword,generateToken,comparePassword}=require('../utils/auth')
 const User=require('../models/userSchema');
 const { asyncHandler, ErrorResponse } = require('../utils/easylife');
 
-const getAllUser=async(req,res)=>{
+const getAllUser=asyncHandler(async(req,res)=>{
 const users=await User.find()
 res.json(users)
-};
+});
+
 const createUser=asyncHandler(async(req,res)=>{/* bereits vorhanden? */
     const {email, username, password}=req.body;
-const hash=hashPassword(password);
+    const hash=await hashPassword(password);
     const userCreate = await User.create({username:username,email:email,password:hash
     });
     const token = generateToken(userCreate._id);
@@ -18,17 +19,21 @@ const hash=hashPassword(password);
 
 const checkUser=asyncHandler(async(req,res)=>{
     const {email, password}=req.body;
-    const foundUser = User.findOne({email:email}).select('+password');
+
+    const foundUser = await User.findOne({email:email,}).select('+password');
+
     if (!foundUser) throw new ErrorResponse("User doesn't exists", 404);
     const isValid = comparePassword(password, foundUser.password);
     if (!isValid) throw new ErrorResponse('Incorrect password', 401);
-    const token = createJWT(foundUser._id);
+    const token = generateToken(foundUser._id);
+
     res.json({ token });
 });
 
 const getUserById=asyncHandler(async(req, res) => {
     const { userId } = req;
     const user = await User.findById(userId);
+
     res.status(201).json(user);
 });
 const updateUserById=(req,res)=>{
